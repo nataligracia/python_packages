e4fda0b57908becd86ccf161e8d1155d9cca9cea
Natali Gracia <ngracia356@gmail.com>
2021-01-19 21:58:39 -0800

    added text files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,4 @@
+#Import python libraries
 import pathlib
 import csv
 
@@ -6,21 +7,48 @@ records_csv = pathlib.Path('/Users/nataligracia/git/python_packages/PyBank/Resou
 
 #Tell python how to read data
 with open (records_csv) as csvfile:
-    csvreader = csv.reader(csvfile)
-    csv_header = next(csvfile)
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
 
-#Labels
-total_months = {}
+    months = 0
+    total_budget = 0
 
 #Loop through data
-for row in csvreader:
+    for row in csvreader:
+        months += 1
+    print(months)
 
-    record_month = row[0]
-    if record_month in total_months
-        total_months[record_month] += 1
-    else:
-        total_months[record_month] = 1
-print(total_month)
+    for row in csvreader:
+        profit_losses = row[1]
+        total_budget = sum(profit_losses)
+    print(total_budget)
+
+    #profit_losses = row[1]
+    #total_profitlosses = 0
+
+    #record_month = row[0]
+    #if record_month in total_months
+    #   total_months[record_month] += 1
+    #else:
+    #   total_months[record_month] = 1
+    #print(total_month)
+
+    #Export script text to file with the results
+    budget_summary = pathlib.Path('/Users/nataligracia/git/python_packages/PyBank/analysis/budget_summary.txt')
+
+    #Formatting summary table for total count, candidate breakdowns, and the winner in terminaal and text file
+    with open(budget_summary,'w') as resultsfile:
+        csvwriter = csv.writer(resultsfile)
+        budget_summary = (
+        f"\n\nFinancial Analysis\n"
+        f"-----------------------------\n"
+        f"Total Months: {months}\n")
+        #f"Total: {total_budget}\n"
+        #f"Average Change: \n"
+        #f"Greatest Increase in Profits: \n"
+        #f"Greatest Decrease in Profits: \n"
+        print(budget_summary, end="")
+        resultsfile.write(budget_summary)
 
 #Total # of months
 #total_months = sum(date)
@@ -31,3 +59,28 @@ print(total_month)
 #Average Change in $ "Profit/Losses"
 #Greatest Increase in Profits (Date and Amount)
 #Greatest Decrease in Losses (Date and Amount)
+
+
+#The total number of months included in the dataset
+
+
+#The net total amount of "Profit/Losses" over the entire period
+
+
+#The average of the changes in "Profit/Losses" over the entire period
+
+
+#The greatest increase in profits (date and amount) over the entire period
+
+
+#The greatest decrease in losses (date and amount) over the entire period
+
+
+#Financial Analysis
+#Total Months: 86
+#Total: $38382578
+#Average  Change: $-2315.12
+#Greatest Increase in Profits: Feb-2012 ($1926159)
+#Greatest Decrease in Profits: Sep-2013 ($-2196167)
+
+#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,66 @@
-#Set python package with library
+#Import python ibraries
 import pathlib
 import csv
 
 #Connect python to data
-votes_csv = pathlib.Path('Resources/PyPoll_Resources_election_data.csv')
+votes_csv = pathlib.Path('/Users/nataligracia/git/python_packages/PyPoll/Resources/PyPoll_Resources_election_data.csv')
 
-#Tell Python how to read csv
-with open(votes_csv) as count_data
-    csvreader = csv.DictReader()
+#Tell python how to read csv
+with open(votes_csv) as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=",")
+    csv_header = next(csvreader)
 
-    for row in 
+    votes_cast = {}
+
+#Start searching data
+    for row in csvreader:
+        vote_candidate = row[2]
+        
+        #Get candidate's unique names
+        if vote_candidate in votes_cast:
+            votes_cast[vote_candidate] += 1
+        else: votes_cast[vote_candidate] = 1
+
+    #Get candidate votes cast counts
+    vote_totals = (list(votes_cast.values()))
+
+    #Get vote total
+    vote_summary = sum(vote_totals)
+
+    #Summary table
+    winner = list(votes_cast.keys())[0]
+    summary_table = {}
+    for vote_candidate in votes_cast.keys():
+        if votes_cast[vote_candidate] > votes_cast[winner]:
+            winner = vote_candidate
+        summary_table[vote_candidate] = {'votes_cast': format(votes_cast[vote_candidate],',d'),'vote_percentage': format((votes_cast[vote_candidate]/vote_summary)*100,".3f")}
+        if vote_candidate == winner:
+            summary_table[vote_candidate]["is_winner"] = True
+        else: summary_table[vote_candidate]["is_winner"] = False
+    
+
+    #Export script text to file with the results
+    ballot_results = pathlib.Path('/Users/nataligracia/git/python_packages/PyPoll/analysis/ballot_results.txt')
+
+    #Formatting summary table for total count, candidate breakdowns, and the winner in terminaal and text file
+    with open(ballot_results,'w') as resultsfile:
+        csvwriter = csv.writer(resultsfile)
+        ballot_results = (
+        f"\n\nElection Results\n"
+        f"-----------------------------\n"
+        f"Total Votes: {format(vote_summary, ',d')}\n"
+        f"-----------------------------\n")
+        print(ballot_results, end="")
+        resultsfile.write(ballot_results)
+
+        for vote_candidate in summary_table.keys():
+            candidate_count = f"{vote_candidate}: {summary_table[vote_candidate]['vote_percentage']}% ({summary_table[vote_candidate]['votes_cast']})\n"
+            print(candidate_count, end= "")
+            resultsfile.write(candidate_count)
+
+        result = (
+        f"-----------------------------\n"
+        f"Winner: {winner}\n"
+        f"-----------------------------\n")
+        print(result)
+        resultsfile.write(result)
